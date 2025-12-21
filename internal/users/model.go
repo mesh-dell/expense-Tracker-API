b1810975ce8e8fc9b821ebfa70cf4a4ed84674e5
mesh-dell <mwaimonicas@gmail.com>
2025-12-21 23:18:01 +0300

    feat: add user service for authentication and registration

--- internal/users/model.go
+++ internal/users/model.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"time"
+
 	"github.com/mesh-dell/expense-Tracker-API/internal/expenses"
 	"gorm.io/gorm"
 )
@@ -8,7 +10,14 @@ import (
 type User struct {
 	gorm.Model
 	Name         string `gorm:"not null"`
-	Email        string `gorm:"not null"`
+	Email        string `gorm:"not null;unique"`
 	PasswordHash string `gorm:"not null"`
 	Expenses     []expenses.Expense
+}
+
+type RefreshToken struct {
+	ID        uint      `gorm:"primaryKey"`
+	UserID    uint      `gorm:"not null;index"`
+	JTI       string    `gorm:"uniqueIndex;size:500;not null"`
+	ExpiresAt time.Time `gorm:"not null"`
 }
---
+++ internal/users/service/user_service.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mesh-dell/expense-Tracker-API/internal/custom"
+	"github.com/mesh-dell/expense-Tracker-API/internal/users"
+	"github.com/mesh-dell/expense-Tracker-API/internal/users/dtos"
+	"github.com/mesh-dell/expense-Tracker-API/internal/users/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService struct {
+	repo repository.IUserRepository
+}
+
+func NewUserRepository(r repository.IUserRepository) *UserService {
+	return &UserService{
+		repo: r,
+	}
+}
+
+func (svc *UserService) Login(ctx context.Context, req dtos.LoginRequest) (*users.User, error) {
+	user, err := svc.repo.FindUserByEmail(ctx, req.Email)
+	if err != nil {
+		return nil, fmt.Errorf("get user error: %v", err)
+	}
+	if user == nil {
+		return nil, custom.ErrInvalidCredentials
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password)); err != nil {
+		return nil, custom.ErrInvalidCredentials
+	}
+	return user, nil
+}
+
+func (svc *UserService) Register(ctx context.Context, req dtos.RegisterRequest) (*users.User, error) {
+	if exists, _ := svc.repo.FindUserByEmail(ctx, req.Email); exists != nil {
+		return nil, custom.ErrEmailAlreadyExists
+	}
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("error hashing password: %v", err)
+	}
+	user := &users.User{
+		Name:         req.Name,
+		Email:        req.Email,
+		PasswordHash: string(passwordHash),
+	}
+	err = svc.repo.CreateUser(ctx, user)
+	if err != nil {
+		return nil, fmt.Errorf("error creating user: %v", err)
+	}
+	return user, nil
+}
