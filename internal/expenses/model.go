529898fa4051d9e86698b477f95d5d801cfb1913
mesh-dell <mwaimonicas@gmail.com>
2025-12-22 20:52:37 +0300

    feat: add expense category model and validation function

--- internal/expenses/model.go
+++ internal/expenses/model.go
@@ -14,3 +14,30 @@ type Expense struct {
 	Amount   float64   `gorm:"not null"`
 	Date     time.Time `gorm:"not null"`
 }
+
+type ExpenseCategory string
+
+const (
+	CategoryGroceries   ExpenseCategory = "Groceries"
+	CategoryLeisure     ExpenseCategory = "Leisure"
+	CategoryElectronics ExpenseCategory = "Electronics"
+	CategoryUtilities   ExpenseCategory = "Utilities"
+	CategoryClothing    ExpenseCategory = "Clothing"
+	CategoryHealth      ExpenseCategory = "Health"
+	CategoryOthers      ExpenseCategory = "Others"
+)
+
+func IsValidCategory(cat string) bool {
+	switch ExpenseCategory(cat) {
+	case CategoryGroceries,
+		CategoryLeisure,
+		CategoryElectronics,
+		CategoryUtilities,
+		CategoryClothing,
+		CategoryHealth,
+		CategoryOthers:
+		return true
+	default:
+		return false
+	}
+}
