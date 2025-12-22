5fc880b268e8ade20d2cec84d6286e98b5517f38
mesh-dell <mwaimonicas@gmail.com>
2025-12-22 21:51:15 +0300

    feat: add error messages for expense not found and invalid category

--- internal/custom/errors.go
+++ internal/custom/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrInvalidCredentials   = errors.New("invalid email or password")
 	ErrEmailAlreadyExists   = errors.New("email already exists")
 	ErrRefreshTokenNotFound = errors.New("refresh token not found")
+	ErrExpenseNotFound      = errors.New("expense not found")
+	ErrInvalidCategory      = errors.New("invalid expense category")
 )
