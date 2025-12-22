dce39fff9e94aed22149a5d78337351c35c9f279
mesh-dell <mwaimonicas@gmail.com>
2025-12-22 21:51:47 +0300

    feat: implement routes for expense operations

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,9 +2,13 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mesh-dell/expense-Tracker-API/internal/api/middleware"
 	"github.com/mesh-dell/expense-Tracker-API/internal/config"
 	"github.com/mesh-dell/expense-Tracker-API/internal/database"
 	"github.com/mesh-dell/expense-Tracker-API/internal/expenses"
+	expensesHandler "github.com/mesh-dell/expense-Tracker-API/internal/expenses/handler"
+	expensesRepository "github.com/mesh-dell/expense-Tracker-API/internal/expenses/repository"
+	expensesService "github.com/mesh-dell/expense-Tracker-API/internal/expenses/service"
 	"github.com/mesh-dell/expense-Tracker-API/internal/users"
 	"github.com/mesh-dell/expense-Tracker-API/internal/users/handler"
 	"github.com/mesh-dell/expense-Tracker-API/internal/users/repository"
@@ -21,7 +25,21 @@ func InitServer(cfg config.Config) {
 	refreshSvc := service.NewRefreshTokenService(refreshRepo)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService, cfg, refreshSvc)
+
+	expensesRepo := expensesRepository.NewExpenseRepository(db)
+	expensesSvc := expensesService.NewExpenseService(expensesRepo)
+	expenseHandler := expensesHandler.NewExpenseHandler(expensesSvc)
+
 	router := gin.Default()
+	protected := router.Group("/expenses")
+	protected.Use(middleware.AuthMiddleware(cfg))
+	{
+		protected.POST("", expenseHandler.Create)
+		protected.GET("/:id", expenseHandler.FindById)
+		protected.GET("", expenseHandler.FindAllForUser)
+		protected.PUT("/:id", expenseHandler.Update)
+		protected.DELETE("/:id", expenseHandler.Delete)
+	}
 	// auth routes
 	router.POST("/login", userHandler.Login)
 	router.POST("/register", userHandler.Register)
